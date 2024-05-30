bf635b52f2e233f26d983f0a9cb07cb5eacf442c
hasan <hasanumurhan@hotmail.com>
2024-05-30 23:53:53 +0300

    Last Commit

--- src/cblog/settings.py
+++ src/cblog/settings.py
@@ -102,7 +102,7 @@ DATABASES = {
         'NAME': 'clarusway',
         'USER': db_username,
         'PASSWORD': db_password,
-        'HOST': 'osman.cj6y4qeaesxo.us-east-1.rds.amazonaws.com',
+        'HOST': 'hasan.cj6y4qeaesxo.us-east-1.rds.amazonaws.com',
         'PORT': '3306',
     }
 }
